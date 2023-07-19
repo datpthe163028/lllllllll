160d304d9bc87904a14a4758fb3df19b8394e9aa
datpthe163028 <datpthe163028@fpt.edu.vn>
2023-07-19 20:28:43 +0700

    first commit

---
+++ Service/ClassError.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Project.Service
+{
+    public class CustomIdentityErrorDescriber : IdentityErrorDescriber
+    {
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateUserName),
+                Description = $""
+            };
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateEmail),
+                Description = $"Email '{email}' đã tồn tại. \n Nếu vẫn không đăng nhập được hãy gửi xác thức lại email ở phần đăng nhập "
+            };
+        }
+    }
+}
